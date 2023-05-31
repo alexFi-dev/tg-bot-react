1018edfe742ce51036ac27cd7fa6f6be8ccc38de
alexFi-dev <alexfi.dev@gmail.com>
2023-06-01 00:13:17 +0700

    continue development

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,15 @@
 import { useEffect } from 'react';
 import './App.css'
 
-const tg = window.Telegram.WebApp
-
 function App() {
 
   useEffect( () => {
     tg.ready()
   }, [])
 
-  const onClose = () => {
-    tg.close()
-  }
-
   return (
     <>
       <div>hello</div>
-      <button onClick={onClose}>close</button>
     </>
   )
 }
