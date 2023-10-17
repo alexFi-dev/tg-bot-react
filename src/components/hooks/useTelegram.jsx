f22217197682b47a76a0586e3096a59b9b0c9ad2
Alexeev Vadim <alexeev.vadim@challenge.day>
2023-10-17 15:23:41 +0700

    expand implamented

--- src/components/hooks/useTelegram.jsx
+++ src/components/hooks/useTelegram.jsx
@@ -8,6 +8,7 @@ export function useTelegram() {
 
     const onToggleButton = () => {
         if(tg.MainButton.isVisible) {
+            tg.expand()
             tg.MainButton.hide()
         } else {
             tg.MainButton.show()
